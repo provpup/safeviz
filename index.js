11afb2a14280c1bc5db1b37c45bbe57018ccf740
Kenny Teng <fof.kenny.teng@gmail.com>
2017-03-14 09:22:23 -0700

    Initial commit
    - Add Radar chart from https://gist.github.com/nbremer/21746a9668ffdf6d8242 and modified it to be compatible with D3.js v4
    - Use Babyparse to read from dataset CSV file
    - Add /students/:student_id route to display radar chart for a given student

---
+++ index.js
@@ -0,0 +1,34 @@
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+let cookieParser = require('cookie-parser');
+let babyParse = require('babyparse');
+
+const PORT = process.env.PORT || 8080;
+
+let results = babyParse.parseFiles('./simulated_data.csv', {header: true, skipEmptyLines: true});
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use('/d3', express.static(__dirname + '/node_modules/d3/build'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+function findStudent(req, res, next) {
+  res.locals.currentStudent = results.data[Number(req.params.id) - 1];
+  next();
+}
+
+app.get('/students/:id', findStudent, function (req, res) {
+  if (res.locals.currentStudent) {
+    res.render('student', Object.assign({id: req.params.id}, res.locals.currentStudent));
+  } else {
+    res.status(404).send(`Student ${req.params.id} not found`);
+  }
+});
+
+app.listen(PORT, function () {
+  console.log('Web server running on port ' + PORT);
+});
